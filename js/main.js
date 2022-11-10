6845480a06ec1295bf9e628ef2ef20ebeb93ea17
ayoungparkme <ayoungparkme@gmail.com>
2022-11-10 09:37:15 +0900

    edit file

--- js/main.js
+++ js/main.js
@@ -22,24 +22,70 @@ const introduceSlider = new Swiper("#introduce .banner", {
   slidesPerView: 1,
   loop: true,
   spaceBetween: 20,
+  centeredSlides: true,
   navigation: {
     nextEl: "#introduce .btn-next",
     prevEl: "#introduce .btn-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+    },
+  },
 });
-
 const activitySlider = new Swiper("#activity  .banner", {
   slidesPerView: 1,
+  slidesPerGroup: 1,
   loop: true,
   spaceBetween: 20,
+
+  //centeredSlides: true,
   navigation: {
     nextEl: "#activity .btn-next",
     prevEl: "#activity .btn-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+  },
 });
-
 const noticeSlider = new Swiper("#notice  .banner", {
-  slidesPerView: 3,
-  direction: "vertical",
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  loop: true,
   spaceBetween: 20,
+
+  //centeredSlides: true,
+  navigation: {
+    nextEl: "#notice .btn-next",
+    prevEl: "#notice .btn-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+});
+const familySlider = new Swiper("footer #family-site .container", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  freeMode: true,
 });
